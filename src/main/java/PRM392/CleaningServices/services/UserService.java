268f9ecb4cb522d674e64f53210d4d13c5d4f728
Shuichi <nguyenthaitoanphuc1307@gmail.com>
2024-10-11 16:21:42 +0700

    Get User By Role

--- src/main/java/PRM392/CleaningServices/services/UserService.java
+++ src/main/java/PRM392/CleaningServices/services/UserService.java
@@ -2,8 +2,11 @@ package PRM392.CleaningServices.services;
 
 import PRM392.CleaningServices.model.User;
 
-public interface UserService {
+import java.util.List;
 
+public interface UserService {
+    List<User> getAllUsers() throws Exception;
     User registerUser(String email, String password, String fullName) throws Exception;
     void updateUserRole(Long userId, String roleName) throws Exception;
+    List<User> getUsersByRole(String roleName) throws Exception;
 }
--- src/main/java/PRM392/CleaningServices/services/impl/UserServiceImpl.java
+++ src/main/java/PRM392/CleaningServices/services/impl/UserServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -24,13 +26,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
     public User registerUser(String email, String password, String fullName) throws Exception {
         if (userRepository.findByEmail(email).isPresent()) {
-            throw new Exception("Email đã tồn tại.");
+            throw new Exception("Email exists.");
         }
         String hashedPassword = passwordEncoder.encode(password);
         Role role = roleRepository.findByRoleName("Customer")
-                .orElseThrow(() -> new Exception("Role CUSTOMER không tồn tại."));
+                .orElseThrow(() -> new Exception("Customer not found."));
         User user = new User();
         user.setEmail(email);
         user.setPassword(hashedPassword);
@@ -42,10 +49,32 @@ public class UserServiceImpl implements UserService {
     @Override
     public void updateUserRole(Long userId, String roleName) throws Exception {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new Exception("Người dùng không tồn tại."));
+                .orElseThrow(() -> new Exception("User not found."));
         Role role = roleRepository.findByRoleName(roleName)
-                .orElseThrow(() -> new Exception("Role không tồn tại."));
+                .orElseThrow(() -> new Exception("Role not found."));
         user.setRole(role);
         userRepository.save(user);
     }
+
+    @Override
+    public List<User> getUsersByRole(String roleName) throws Exception {
+        List<User> users;
+        switch (roleName) {
+            case "Admin":
+                users = userRepository.findByRole_RoleName("Admin");
+                break;
+            case "Customer":
+                users = userRepository.findByRole_RoleName("Customer");
+                break;
+            case "Staff":
+                users = userRepository.findByRole_RoleName("Staff");
+                break;
+            default:
+                throw new Exception("Role invalid!");
+        }
+
+        return users;
+    }
+
+
 }
