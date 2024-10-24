493b1743e1ffc742ef50d1f4ce2b6e083ddf8aef
Shuichi <nguyenthaitoanphuc1307@gmail.com>
2024-10-24 11:21:00 +0700

    Update User

--- src/main/java/PRM392/CleaningServices/dto/reponse/LoginRespone.java
+++ src/main/java/PRM392/CleaningServices/dto/reponse/LoginRespone.java
@@ -1,4 +1,27 @@
 package PRM392.CleaningServices.dto.reponse;
 
+import PRM392.CleaningServices.model.Role;
+
 public class LoginRespone {
+    private String token;
+    private String email;
+    private Role role; // Thay vì String, sử dụng Role
+
+    public LoginRespone(String token, String email, Role role) {
+        this.token = token;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
 }
